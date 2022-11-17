a43459a580ca0dad5eb92dc6296d61abe022b170
jordanestenanderson <jordan@esten.co>
2022-11-17 14:02:14 -0700

    Removed bulk of Esten.co site

--- _site/js/base.js
+++ _site/js/base.js
@@ -1,17 +1,8 @@
-var hueHistory = ['#1b1b1b'];
-
 $(".burger-wrap").click(function(){
   $("html, .overlay, .burger").toggleClass("open");
 });
 
 function BaseJS() {
-	$(document).on("click",".loader-link",function() {
-        var hex = $(this).data('hue');
-        hueHistory.push(hex);
-		console.log(hueHistory);
-        $("#wipe, .loader").css({background:hex});
-	});
-
 	AOS.init({
 	  	duration: 1000,
 	    easing: 'ease-out-quad',
