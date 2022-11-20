17a01577a76b46b5cfe8bc13e5584266c2aef3c0
jordanestenanderson <jordan@esten.co>
2022-11-19 23:11:15 -0700

    setup base styles and started homepage

--- _site/js/base.js
+++ _site/js/base.js
@@ -1,14 +1,7 @@
-$(".burger-wrap").click(function(){
-  $("html, .overlay, .burger").toggleClass("open");
-});
+AOS.init({
+  	duration: 1000,
+    easing: 'ease-out-quad',
+    once: true,
+})
 
-function BaseJS() {
-	AOS.init({
-	  	duration: 1000,
-	    easing: 'ease-out-quad',
-	    once: true,
-	})
-
-	document.getElementById("year").innerHTML = new Date().getFullYear();
-}
-BaseJS();
+document.getElementById("year").innerHTML = new Date().getFullYear();
