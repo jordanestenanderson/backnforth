7b2aeaa6c03b66645e48a3cc4fc59252b6c4e990
jordanestenanderson <jordan@esten.co>
2023-01-11 17:57:20 -0700

    &

--- _site/js/base.js
+++ _site/js/base.js
@@ -4,4 +4,18 @@ AOS.init({
     once: true,
 })
 
+$(document).ready(function(){
+  $('.step-graphic').each(function(){
+   var triggerValue = $(this).attr('trigger');
+   $(this).click(function(){
+      $("body, .lightbox, .overlay, .lightbox-content").addClass("open");
+      $("." + triggerValue).addClass("open");
+   });
+  });
+});
+
+$(".lightbox-close, .lightbox-scroller").click(function(){
+   $("body, .lightbox, .overlay, .lightbox-content, .lightbox-step").removeClass("open");
+});
+
 document.getElementById("year").innerHTML = new Date().getFullYear();
